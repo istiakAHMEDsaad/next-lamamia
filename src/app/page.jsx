ca072da4b26f8de64d865866120d53448e4ed9e9
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-09 17:08:10 +0600

    working on optimizing external iamge

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,46 @@
+import Image from 'next/image';
+import Hero from '@/assets/hero.png';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* text */}
+      <div>
+        <h1>Perfect design for your personal projects</h1>
+        <p>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className='px-5.5 py-2 rounded bg-emerald-600 text-white hover:bg-emerald-500 transition-colors cursor-pointer'>
+          Find More
+        </button>
+      </div>
+
+      {/* image container */}
+      <div>
+        <Image src={Hero} alt='hero image' />
+      </div>
+    </div>
+  );
 }
+
+/*
+use fill method
+<div className='relative w-96 h-96'>
+        <Image
+          src='https://images.unsplash.com/photo-1744024450738-b040cffc7ff9?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt='unsplash stock image'
+          fill
+          className='object-cover'
+        />
+      </div>
+
+normal method
+<Image
+        src='https://images.pexels.com/photos/34341418/pexels-photo-34341418.jpeg'
+        alt='pexels stock image'
+        width={500}
+        height={500}
+        className='h-[400px] w-[400px] object-cover rounded mt-10'
+      />
+*/
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,32 @@
+import Image from 'next/image';
+
 const Footer = () => {
-  return <div>&copy;2025 istiakAHMEDsaad. All rights reserved.</div>;
+  const imageLocal = [
+    { id: 1, src: '/1.png' },
+    { id: 2, src: '/2.png' },
+    { id: 3, src: '/3.png' },
+    { id: 4, src: '/4.png' },
+  ];
+
+  return (
+    <div className='h-12 flex flex-row items-center justify-between text-sm'>
+      <div>&copy;2025 istiakAHMEDsaad. All rights reserved.</div>
+
+      {/* image logo */}
+      <div className='flex items-center gap-2.5'>
+        {imageLocal.map((item) => (
+          <Image
+            key={item.id}
+            src={item.src}
+            width={15}
+            height={15}
+            alt='logo'
+            className='opacity-80 cursor-pointer hover:opacity-100 transition-opacity'
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
