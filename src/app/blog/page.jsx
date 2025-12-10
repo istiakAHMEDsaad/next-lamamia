70637c7823f5357e59c76cfd4e5c71bc969de9d1
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-11 01:04:58 +0600

    blog card

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,20 @@
+import BlogCard from '@/components/cards/BlogCard';
+
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className='flex flex-col space-y-16'>
+      <BlogCard
+        imgSrc='https://images.pexels.com/photos/3156381/pexels-photo-3156381.jpeg'
+        titleBlog='This is it'
+        descBlog='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta error accusantium voluptates reiciendis ipsam dolores dolor eligendi aut nihil saepe.'
+      />
+      <BlogCard
+        imgSrc='https://images.pexels.com/photos/29170205/pexels-photo-29170205.jpeg'
+        titleBlog='This is it'
+        descBlog='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta error accusantium voluptates reiciendis ipsam dolores dolor eligendi aut nihil saepe.'
+      />
+    </div>
+  );
 };
 
 export default Blog;
---
+++ src/components/cards/BlogCard.jsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+const BlogCard = ({ imgSrc, titleBlog, descBlog }) => {
+  return (
+    <>
+      <Link href='#'>
+        <div className='flex flex-col lg:flex-row justify-between max-sm:items-center max-sm:gap-4'>
+          {/* left image */}
+          <div className='lg:basis-[40%] max-md:flex-1'>
+            <div className='relative w-72 h-[162px] md:w-[500px] md:h-[281.25px]'>
+              <Image
+                src={imgSrc}
+                fill
+                alt='image'
+                className='object-cover rounded'
+              />
+            </div>
+          </div>
+
+          {/* right text */}
+          <div className='lg:basis-[55%] max-md:flex-1'>
+            <div className='flex flex-col gap-5'>
+              <h1 className='text-3xl font-semibold'>{titleBlog}</h1>
+
+              <p>{descBlog.slice(0, 15)}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default BlogCard;
