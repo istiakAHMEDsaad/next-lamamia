d2e7248ea346acb8a1303886202490eb529a6cca
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-17 01:49:32 +0600

    client side fetching using normal useEffect

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,5 +1,53 @@
+'use client';
+import { useEffect, useState } from 'react';
+import LoaderSpinner from '@/components/loader/LoaderSpinner';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+  //       next: { revalidate: 120 },
+  //     });
+
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
+
+  //     const data = await res.json();
+
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+
+  //   getData();
+  // }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <div className='h-screen flex items-center justify-center'>
+          <LoaderSpinner />
+        </div>
+      ) : (
+        <div className='flex flex-col gap-5'>
+          {data.map((item) => (
+            <div
+              key={item.id}
+              className='w-full h-30 border border-gray-500 rounded-md px-4 py-2'
+            >
+              <p className='text-2xl font-bold'>{item.title}</p>
+              <p className='text-sm text-gray-500'>{item.body}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
