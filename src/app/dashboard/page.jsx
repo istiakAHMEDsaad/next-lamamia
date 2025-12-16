1f1574ce141155354963cabfe890f2a17278677a
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-17 02:02:58 +0600

    client side data fetching using swr

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,31 +1,14 @@
 'use client';
 import { useEffect, useState } from 'react';
 import LoaderSpinner from '@/components/loader/LoaderSpinner';
+import useSWR from 'swr';
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
-  const [err, setErr] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     setIsLoading(true);
-  //     const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-  //       next: { revalidate: 120 },
-  //     });
-
-  //     if (!res.ok) {
-  //       setErr(true);
-  //     }
-
-  //     const data = await res.json();
-
-  //     setData(data);
-  //     setIsLoading(false);
-  //   };
-
-  //   getData();
-  // }, []);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    'https://jsonplaceholder.typicode.com/posts',
+    fetcher
+  );
 
   return (
     <div>
@@ -51,3 +34,28 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+/*const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        next: { revalidate: 120 },
+      });
+
+      if (!res.ok) {
+        setErr(true);
+      }
+
+      const data = await res.json();
+
+      setData(data);
+      setIsLoading(false);
+    };
+
+    getData();
+  }, []);
+  */
