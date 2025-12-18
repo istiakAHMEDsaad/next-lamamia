a581771ba45ab12d5ace1d5ed45f3ff44f61498d
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-18 23:53:07 +0600

    working on backend

---
+++ src/models/Post.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    }
+  },
+  { timestamps: true }
+);
